281340b9a59dd1338d2933b5e6edafb877b64664
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2024-09-24 21:11:40 +0530

    save post done

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   getSingleUser,
   getUsers,
+  savePost,
   updateUser,
 } from "../controller/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
@@ -13,5 +14,6 @@ router.get("/", getUsers);
 router.get("/:id", verifyToken, getSingleUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete(":id", verifyToken, deleteUser);
+router.post("/save", verifyToken, savePost);
 
 export default router;
